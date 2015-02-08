da3bf79ddb0004c5e44726ea94a414200874a58f
jqson <yuanfengjiao@gmail.com>
2015-02-07 18:45:20 -0800

    change background based on sun and cloud

--- weather.js
+++ weather.js
@@ -20,10 +20,20 @@ $(document).ready(function() {
     updateWeather();
     setInterval(updateWeather, weatherUpdate * 1000);
   });
+
+  $('.cities-list').css('display', 'block')
 })  
 
- 
-function getLocalDate(time, offset) {
+// convet UNIX time to local time
+function UNIXtoLocalDate(time, offset) {
+  var date = new Date(time * 1000);
+  var utc = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes());
+  utc.setHours(utc.getHours() + offset);
+  return utc;
+}
+
+// convert UTC time to local time
+function UTCtoLocalDate(time, offset) {
   var date = new Date(time);
   var utc = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes());
   utc.setHours(utc.getHours() + offset);
@@ -33,8 +43,8 @@ function getLocalDate(time, offset) {
 // convert time to h:mm A format and update
 function updateTime() {
   var curTime = new Date($.now());
-  var sanjoseTime= getLocalDate($.now(), -8);
-  var sydneyTime = getLocalDate($.now(), 11);
+  var sanjoseTime= UTCtoLocalDate($.now(), -8);
+  var sydneyTime = UTCtoLocalDate($.now(), 11);
   getTime(curTime, 'is-cur');
   getTime(sanjoseTime, 'is-sanjose');
   getTime(sydneyTime, 'is-sydney');
@@ -42,20 +52,25 @@ function updateTime() {
   function getTime(time, city) {
     var timeString = time.toLocaleTimeString();
     timeString = timeString.substring(0, timeString.lastIndexOf(':')) + timeString.substr(timeString.indexOf(' '), 3);
-    //var timeString = date.toLocaleTimeString();
+    // save time to localStroage
+    if (typeof(Storage) !== 'undefined') {
+      localStorage.setItem(city + '-time', timeString);
+    }
     $('#' + city + ' .time').text(timeString);
   }
 }
 
 function updateWeather() {
-  navigator.geolocation.getCurrentPosition(curLoc);
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(curLoc);
 
-  function curLoc(position) {
-    var curLat = position.coords.latitude;
-    var curLong = position.coords.longitude;
-    //console.log(curLat);
-    //console.log(curLong);
-    getWeather(curLat, curLong, 'is-cur');
+    function curLoc(position) {
+      var curLat = position.coords.latitude;
+      var curLong = position.coords.longitude;
+      //console.log(curLat);
+      //console.log(curLong);
+      getWeather(curLat, curLong, 'is-cur');
+    }
   }
 
   getWeather(sanjoseLat, sanjoseLong, 'is-sanjose');
@@ -64,15 +79,47 @@ function updateWeather() {
   function getWeather(latitude, longitude, city) {
     $.ajax({
       type: 'GET',
-      url: 'https://api.forecast.io/forecast/' + apikey + '/' + latitude + ',' + longitude,
+      url: 'https://api.forecast.io/forecast/' + apikey + '/' + latitude + ',' + longitude + '?units=si',
       dataType: 'jsonp',
       crossDomain: true,
-    }).done(function(result) {
-      console.log(result);
-      var curTemp = Math.round(result.currently.temperature);
-      console.log(curTemp);
+    }).done(function(weather) {
+      console.log(weather);
+      // save weather to local stroage
+      localStorage.setItem(city + '-weather', weather);
+
+      // get Temperature
+      var curTemp = Math.round(weather.currently.temperature);
+      //console.log(curTemp);
+       
       $('#' + city + ' .city-temp').text(curTemp + '˚');
+      
+      // get sunrise/sunset time and determin day/night
+      var curTime = UNIXtoLocalDate(weather.currently.time, weather.offset);
+      var sunriseTime = UNIXtoLocalDate(weather.daily.data[0].sunriseTime, weather.offset);
+      var sunsetTime = UNIXtoLocalDate(weather.daily.data[0].sunsetTime, weather.offset);
+      var day = curTime > sunriseTime && curTime < sunsetTime;
+      
+      // get cloudy or not
+      var cloudy = weather.currently.cloudCover > 0.5;
+      
+      // determin background color
+      var bgClass = 'day';
+      if (day) {
+        if (cloudy) bgClass = 'dayCloudy';
+        else bgClass = 'day';
+      }
+      else {
+        if (cloudy) bgClass = 'nightCloudy';
+        else bgClass = 'night';
+      }
+
+      $('#' + city).attr('class', bgClass);
     });
   }
 }
+
+function loadTime() {
+
+}
+
 
